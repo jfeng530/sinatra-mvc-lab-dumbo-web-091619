9fdcbff1cb473ecc18ae553da4bfcfd1ce35bd2c
Jacky Feng <jackyfeng530@yahoo.com>
2019-10-08 17:12:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end 
+  
+  post '/piglatin' do
+    pl = PigLatinizer.new
+    @phrase = pl.piglatinize(params[:words])
+    erb :result
+  end
 end
